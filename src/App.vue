9091ff1678a615c0a75611161d4609198b565a38
Omann07 <innimman@gmail.com>
2024-09-12 22:09:35 +0700

    box color

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,18 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+const boxColor = ref('grey');
+function changeColor() {
+  const colors = ['red', 'green', 'blue', 'yellow'];
+  boxColor.value = colors[Math.floor(Math.random() * colors.length)];
+}
 </script>
 
 <template>
   {{ count }}
   <button @click="count++">Count</button>
+
+  
+    <button @click="changeColor" >Ubah Warna Box</button>
+    <div :style="{ backgroundColor: boxColor, width: '150px', height: '150px' }"></div>
 </template>
